c5d4e851d4c143845b7bce9f3873b177c17fa69e
Ben Ransford <ben@ransford.org>
2013-07-02 11:47:13 -0400

    wrapper script for main GUI app

---
+++ wisp/gui.py
@@ -0,0 +1,13 @@
+import wisp.exceptions
+import argparse
+import os
+import sys
+import logging
+from PyQt4 import Qt
+from PyQt4 import QtGui, QtCore
+
+def main ():
+    print "Hello world!"
+
+if __name__ == '__main__':
+    main()
